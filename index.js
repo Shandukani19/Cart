00ee71c2eef3d5a52877b84c596af34b039588e8
Shandukani19 <mudzwirisa@gmail.com>
2024-07-19 08:41:34 +0200

    added and updated items into the cart div

--- index.js
+++ index.js
@@ -1,53 +1,65 @@
 let cart = [];
-function addToCart(name, price){
-  let existingItem = cart.find(item => item.name === name);
-  if(existingItem){
+function addToCart(name, price, image) {
+  let existingItem = cart.find((item) => item.name === name);
+  if (existingItem) {
     existingItem.quantity += 1;
-} else {
-  cart.push({name, price, quantity: 1});
-} 
-updateCartCount();
+  } else {
+    cart.push({ name, price, image, quantity: 1 });
+  }
+  updateCartCount();
+  displayCartItems();
+  updateTotalAmount();
 }
 
-function updateCartCount(){
-  let cartCount = document.getElementById('cart-count');
+function updateCartCount() {
+  let cartCount = document.getElementById("cart-count");
   let totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
   cartCount.textContent = totalItems;
 }
 
-function cartDetails(){
-  let cartDetails = document.getElementById('cart-details');
-  cartDetails.style.display = 'block';
+function cartDetails() {
+  let cartDetails = document.getElementById("cart-details");
+  cartDetails.style.display = "block";
   displayCartItems();
 }
 
-function displayCartItems(){
-  let cartItems = document.getElementById('cart-items');
-  cartItems.innerHTML = '';
-  cart.forEach(item => {
-    const itemDiv =document.createElement('div');
-    itemDiv.className = 'cart-item';
-    itemDiv.innerHTML = `<p>${item.name} - ${item.price} x ${item.quantity}</p> 
-    <button onClick = 'removefromCart('${item.name}')'> remove </button> `;
-    cartItems.appendChild(itemDiv);
-  })
+function closeCart() {
+  let cartDetails = document.getElementById("cart-details");
+  cartDetails.style.display = "none";
 }
 
-function removefromCart(name){
-  let itemIndex = cart.findIndex(item => item.name === name);
-  if(itemIndex > -1){
+function displayCartItems() {
+  let cartItems = document.getElementById("cart-items");
+  cartItems.innerHTML = "";
+  cart.forEach((item) => {
+    const itemDiv = document.createElement("div");
+    itemDiv.className = "cart-item";
+
+    itemDiv.innerHTML = `<p> <img src="${item.image}"> ${item.name} - ${item.price} x ${item.quantity}</p> 
+    <button onClick = 'decreaseQuantity()' > - </button> 
+    <button onClick = 'addToCart('${item.name}')'> + </button>`;
+    cartItems.appendChild(itemDiv);
+  });
+}
+
+//
+function removefromCart(name) {
+  let itemIndex = cart.findIndex((item) => item.name === name);
+  if (itemIndex > -1) {
     cart[itemIndex].quantity--;
-    if(cart[itemIndex].quantity === 0){
+    if (cart[itemIndex].quantity === 0) {
       cart.splice(itemIndex, 1);
     }
     updateCartCount();
     displayCartItems();
-    }
-  
+  }
 }
 
-function updateTotalAmount(){
-  let totalAmountSpan = document.getElementById('total-amount');
-  let totalAmount = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+function updateTotalAmount() {
+  let totalAmountSpan = document.getElementById("total-amount");
+  let totalAmount = cart.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0
+  );
   totalAmountSpan.textContent = totalAmount.toFixed(2);
 }
